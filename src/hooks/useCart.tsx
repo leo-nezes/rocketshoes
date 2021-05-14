7dc501058b9d763b0bf89e960dd8252dfc7bb415
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-05-14 16:51:36 -0300

    feat: Carregando produtos - Buscando e renderizando produtos do JSON server - Iniciando estado do hook useCart - Implementando parte do código para adicionar produtos

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,27 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const response = await api.get(`/products/${productId}`);
+
+      if(!response.data) throw new Error();
+
+      const newProducts = [...cart, response.data];
+      
+      setCart([...newProducts]);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newProducts));
     } catch {
-      // TODO
+      
     }
   };
 
