5a56b1955f89a2d5b0febdfaf3378037085d2180
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-05-19 17:15:18 -0300

    test: Finishing all tests suites Header: - should be able to render the amount of products added to cart Home: - should be able to render each product quantity added to cart - should be able to add a product to cart Cart: - should be able to increase/decrease a product amount - should not be able to decrease a product amount when value is 1 - should be able to remove a product useCart: - should not be able to update a product amount when running out of stock

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,12 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const newSumAmount = { ...sumAmount };
+    newSumAmount[product.id] = product.amount;
+
+    return newSumAmount;
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -36,10 +39,7 @@ const Home = (): JSX.Element => {
       const products = response.data.map((product) => {
         return {
           ...product,
-          priceFormatted: new Intl.NumberFormat('pt-BR', {
-                            style: 'currency',
-                            currency: 'BRL'
-                          }).format(product.price)
+          priceFormatted: formatPrice(product.price)
         }
       });
 
@@ -56,7 +56,7 @@ const Home = (): JSX.Element => {
   return (
     <ProductList>
       {products.map(product => (
-        <li>
+        <li key={product.id}>
           <img src={product.image} alt={product.title} />
           <strong>{product.title}</strong>
           <span>{product.priceFormatted}</span>
@@ -67,14 +67,14 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
           </button>
         </li>
       ))}
-      
+
     </ProductList>
   );
 };
